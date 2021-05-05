b3a3d7b1ce6a790dfd2e37ed1d21fb3982d4b863
Arik Sosman <git@arik.io>
2021-05-05 00:03:43 -0700

    create experimental xcframework builder

---
+++ xcode/LDKFramework/LDKFramework/LDKExampleClass.swift
@@ -0,0 +1,16 @@
+//
+//  LDKExampleClass.swift
+//  LDKFramework
+//
+//  Created by Arik Sosman on 5/5/21.
+//
+
+import Foundation
+
+public class LDKExampleClass {
+    
+    static func printSomething() -> Void {
+        print("hello world!")
+    }
+    
+}
---
+++ xcode/LDKFramework/LDKFramework/LDKFramework.h
@@ -0,0 +1,18 @@
+//
+//  LDKFramework.h
+//  LDKFramework
+//
+//  Created by Arik Sosman on 5/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LDKFramework.
+FOUNDATION_EXPORT double LDKFrameworkVersionNumber;
+
+//! Project version string for LDKFramework.
+FOUNDATION_EXPORT const unsigned char LDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDKFramework/PublicHeader.h>
+
+

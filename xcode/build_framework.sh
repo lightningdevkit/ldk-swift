9174e81f6f4a8f8f19c04cad2b2a33bd4c348422
Arik Sosman <git@arik.io>
2021-05-05 21:43:08 -0700

    make xcframework simulator-compatible

--- xcode/build_framework.sh
+++ xcode/build_framework.sh
@@ -1,10 +1,10 @@
 pushd ../bindings/bin
 BIN_OUTPUT_DIRECTORY=`pwd`
-# rm -r LDKFramework*
+rm -r LDKFramework*
 popd
 
 pushd ./LDKFramework
-#xcodebuild archive -scheme LDKFramework -destination "generic/platform=iOS" -archivePath ${BIN_OUTPUT_DIRECTORY}/LDKFramework-iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme LDKFramework -destination "generic/platform=iOS" -archivePath ${BIN_OUTPUT_DIRECTORY}/LDKFramework-iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 xcodebuild archive -scheme LDKFramework -destination "generic/platform=iOS Simulator" -archivePath ${BIN_OUTPUT_DIRECTORY}/LDKFramework-Sim SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 #xcodebuild archive -scheme LDKFramework -destination "generic/platform=OS X" -archivePath ${BIN_OUTPUT_DIRECTORY}/LDKFramework-macOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -15,7 +15,7 @@ xcodebuild archive -scheme LDKFramework -destination "generic/platform=iOS Simul
 #-framework ${BIN_OUTPUT_DIRECTORY}/LDKFramework-macOS.xcarchive/Products/Library/Frameworks/LDKFramework.framework \
 #-output ${BIN_OUTPUT_DIRECTORY}/LDKFramework.xcframework
 
-#xcodebuild -create-xcframework \
-#-framework ${BIN_OUTPUT_DIRECTORY}/LDKFramework-iOS.xcarchive/Products/Library/Frameworks/LDKFramework.framework \
-#-framework ${BIN_OUTPUT_DIRECTORY}/LDKFramework-macOS.xcarchive/Products/Library/Frameworks/LDKFramework.framework \
-#-output ${BIN_OUTPUT_DIRECTORY}/LDKFramework.xcframework
+xcodebuild -create-xcframework \
+-framework ${BIN_OUTPUT_DIRECTORY}/LDKFramework-iOS.xcarchive/Products/Library/Frameworks/LDKFramework.framework \
+-framework ${BIN_OUTPUT_DIRECTORY}/LDKFramework-Sim.xcarchive/Products/Library/Frameworks/LDKFramework.framework \
+-output ${BIN_OUTPUT_DIRECTORY}/LDKFramework.xcframework

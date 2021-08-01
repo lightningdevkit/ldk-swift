bd8980d226cff3f3999f31d033ac6dcca92bc25e
Arik Sosman <git@arik.io>
2021-08-01 02:18:35 -0700

    properly format  Package.swift and add asan linked library

--- ci/LDKSwift/Package.swift
+++ ci/LDKSwift/Package.swift
@@ -5,69 +5,58 @@ import PackageDescription
 import Foundation
 
 let package = Package(
-    name: "LDKSwift",
-    products: [
-       .library(
-           name: "LDKSwift",
-           targets: ["LDKSwift"]),
-//         .library(name: "LDKSwift", type: .dynamic, targets: ["LDKSwift"])
-    ],
-    dependencies: [],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "LDKSwift",
-//            dependencies: ["LDKCHeaders"],
-//            linkerSettings: [
-//                 .linkedLibrary(String(utf8String: getenv("LDK_C_BINDINGS_BASE")!)! + "/lightning-c-bindings/target/debug/libldk.a"),
-//                 .linkedLibrary(String(utf8String: getenv("LDK_C_BINDINGS_BASE")!)! + "/lightning-c-bindings/ldk_net.o"),
-//                 .linkedLibrary("/usr/lib/llvm-11/lib/clang/11.0.0/lib/linux/libclang_rt.asan-x86_64.a")
-//            ]),
-
-
-        .target(name: "LDKHeaders"),
-// 		.systemLibrary(name: "LDKHeaders"),
-        .target(
-            name: "LDKSwift",
-            dependencies: [
-                "LDKHeaders"
-            ],
-            path: nil,
-            exclude: [],
-           	sources: nil,
-//             sources: [
-//                 "ldk_net.h",
-//                 "ldk_net.c",
-//                 "ldk_rust_types.h",
-//                 "ldk_ver.h",
-//                 "lightning.h",
-//                 "SanitySample.swift"
-//             ],
-            resources: nil,
-            publicHeadersPath: "include",
-            cSettings: nil,
-//            cSettings: [
-//                .headerSearchPath("include"),
-//            ],
-            cxxSettings: nil,
-            swiftSettings: nil,
-            linkerSettings: [
-                .linkedLibrary(String(utf8String: getenv("LDK_C_BINDINGS_BASE")!)! + "/lightning-c-bindings/target/debug/libldk.a")
-            ]),
-//        .testTarget(
-//            name: "LDKSwiftTests",
-//            dependencies: ["LDKSwift"]),
-        .testTarget(name: "LDKSwiftTests", dependencies: ["LDKSwift", "LDKHeaders"], path: nil,
-//         	exclude: [],
-        	exclude: ["SampleTest.swift"],
+        name: "LDKSwift",
+        products: [
+            .library(
+                    name: "LDKSwift",
+                    targets: ["LDKSwift"]),
+            // .library(name: "LDKSwift", type: .dynamic, targets: ["LDKSwift"])
+        ],
+        dependencies: [],
+        targets: [
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .target(name: "LDKHeaders"),
+            // .systemLibrary(name: "LDKHeaders"),
+            .target(
+                    name: "LDKSwift",
+                    dependencies: [
+                        "LDKHeaders"
+                    ],
+                    path: nil,
+                    exclude: [],
                     sources: nil,
-//                     sources: [
-//                         "SampleTest.swift"
-//                     ],
-                    cSettings: nil, cxxSettings: nil, swiftSettings: nil, linkerSettings: [
-					  .linkedLibrary(String(utf8String: getenv("LDK_C_BINDINGS_BASE")!)! + "/lightning-c-bindings/target/debug/libldk.a")
-				  ])
+                    // sources: [
+                    //     "ldk_net.h",
+                    //     "ldk_net.c",
+                    //     "ldk_rust_types.h",
+                    //     "ldk_ver.h",
+                    //     "lightning.h",
+                    //     "SanitySample.swift"
+                    // ],
+                    resources: nil,
+                    publicHeadersPath: "include",
+                    cSettings: nil,
+                    // cSettings: [
+                    //     .headerSearchPath("include"),
+                    // ],
+                    cxxSettings: nil,
+                    swiftSettings: nil,
+                    linkerSettings: [
+                        .linkedLibrary(String(utf8String: getenv("LDK_C_BINDINGS_BASE")!)! + "/lightning-c-bindings/target/debug/libldk.a"),
+                        .linkedLibrary("/usr/lib/llvm-11/lib/clang/11.0.0/lib/linux/libclang_rt.asan-x86_64.a")
+                    ]),
+            .testTarget(name: "LDKSwiftTests", dependencies: ["LDKSwift", "LDKHeaders"], path: nil,
+                    exclude: [],
+                    // exclude: ["SampleTest.swift"],
+                    sources: nil,
+                    // sources: [
+                    //     "SampleTest.swift"
+                    // ],
+                    cSettings: nil, cxxSettings: nil, swiftSettings: nil,
+                    linkerSettings: [
+                        .linkedLibrary(String(utf8String: getenv("LDK_C_BINDINGS_BASE")!)! + "/lightning-c-bindings/target/debug/libldk.a")
+                    ])
 
-    ]
+        ]
 )

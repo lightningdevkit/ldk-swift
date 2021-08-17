9e99325b7105758a338f9046e441387bde6bd656
Arik Sosman <git@arik.io>
2021-08-17 10:14:23 -0700

    make userconfig deinitable again

--- bindings/LDK/structs/UserConfig.swift
+++ bindings/LDK/structs/UserConfig.swift
@@ -6,7 +6,7 @@ public class UserConfig {
 
 	/* DEFAULT_CONSTRUCTOR_START */
     public init() {
-    	
+
         self.cOpaqueStruct = UserConfig_default()
     }
     /* DEFAULT_CONSTRUCTOR_END */
@@ -18,86 +18,86 @@ public class UserConfig {
     /* STRUCT_METHODS_START */
 
     public func get_own_channel_config() -> ChannelHandshakeConfig {
-    	
+
         return ChannelHandshakeConfig(pointer: withUnsafePointer(to: self.cOpaqueStruct!) { (this_ptrPointer: UnsafePointer<LDKUserConfig>) in
 UserConfig_get_own_channel_config(this_ptrPointer)
 });
     }
 
     public func set_own_channel_config(val: ChannelHandshakeConfig) -> Void {
-    	
+
 							let this_ptrPointer = UnsafeMutablePointer<LDKUserConfig>.allocate(capacity: 1)
 							this_ptrPointer.initialize(to: self.cOpaqueStruct!)
-						
+
         return UserConfig_set_own_channel_config(this_ptrPointer, val.clone().cOpaqueStruct!);
     }
 
     public func get_peer_channel_config_limits() -> ChannelHandshakeLimits {
-    	
+
         return ChannelHandshakeLimits(pointer: withUnsafePointer(to: self.cOpaqueStruct!) { (this_ptrPointer: UnsafePointer<LDKUserConfig>) in
 UserConfig_get_peer_channel_config_limits(this_ptrPointer)
 });
     }
 
     public func set_peer_channel_config_limits(val: ChannelHandshakeLimits) -> Void {
-    	
+
 							let this_ptrPointer = UnsafeMutablePointer<LDKUserConfig>.allocate(capacity: 1)
 							this_ptrPointer.initialize(to: self.cOpaqueStruct!)
-						
+
         return UserConfig_set_peer_channel_config_limits(this_ptrPointer, val.clone().cOpaqueStruct!);
     }
 
     public func get_channel_options() -> ChannelConfig {
-    	
+
         return ChannelConfig(pointer: withUnsafePointer(to: self.cOpaqueStruct!) { (this_ptrPointer: UnsafePointer<LDKUserConfig>) in
 UserConfig_get_channel_options(this_ptrPointer)
 });
     }
 
     public func set_channel_options(val: ChannelConfig) -> Void {
-    	
+
 							let this_ptrPointer = UnsafeMutablePointer<LDKUserConfig>.allocate(capacity: 1)
 							this_ptrPointer.initialize(to: self.cOpaqueStruct!)
-						
+
         return UserConfig_set_channel_options(this_ptrPointer, val.clone().cOpaqueStruct!);
     }
 
     public func get_accept_forwards_to_priv_channels() -> Bool {
-    	
+
         return withUnsafePointer(to: self.cOpaqueStruct!) { (this_ptrPointer: UnsafePointer<LDKUserConfig>) in
 UserConfig_get_accept_forwards_to_priv_channels(this_ptrPointer)
 };
     }
 
     public func set_accept_forwards_to_priv_channels(val: Bool) -> Void {
-    	
+
 							let this_ptrPointer = UnsafeMutablePointer<LDKUserConfig>.allocate(capacity: 1)
 							this_ptrPointer.initialize(to: self.cOpaqueStruct!)
-						
+
         return UserConfig_set_accept_forwards_to_priv_channels(this_ptrPointer, val);
     }
 
     public func clone() -> UserConfig {
 
-        let thingz: UnsafeRawPointer = UnsafeRawPointer(&self.cOpaqueStruct!)
-        let ugh: UnsafePointer<LDKUserConfig> = thingz.assumingMemoryBound(to: LDKUserConfig.self)
-    	
-        // return withUnsafePointer(to: self.cOpaqueStruct!) { (origPointer: UnsafePointer<LDKUserConfig>) in UserConfig(pointer: UserConfig_clone(origPointer)) };
-        let clone = UserConfig(pointer: UserConfig_clone(ugh))
-        UserConfig_free(self.cOpaqueStruct!)
-        return clone
-    }
+		let thingz: UnsafeRawPointer = UnsafeRawPointer(&self.cOpaqueStruct!)
+		let ugh: UnsafePointer<LDKUserConfig> = thingz.assumingMemoryBound(to: LDKUserConfig.self)
 
-				
-	deinit {
-					
-					
-					
+		// return withUnsafePointer(to: self.cOpaqueStruct!) { (origPointer: UnsafePointer<LDKUserConfig>) in UserConfig(pointer: UserConfig_clone(origPointer)) };
+		let clone = UserConfig(pointer: UserConfig_clone(ugh))
 		// UserConfig_free(self.cOpaqueStruct!)
-					
-				
+		return clone
 	}
-			
+
+
+	deinit {
+
+
+
+		UserConfig_free(self.cOpaqueStruct!)
+
+
+	}
+
     /* STRUCT_METHODS_END */
 
 }

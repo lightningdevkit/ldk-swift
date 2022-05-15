a53a8291e7fa694cf9c477a3886618882ddbb363
Jurvis Tan <hello@jurvis.co>
2022-05-14 18:09:43 -0700

    Create new LDK Xcode project

---
+++ xcode/LDK/LDK/LDK.h
@@ -0,0 +1,18 @@
+//
+//  LDK.h
+//  LDK
+//
+//  Created by Jurvis on 5/14/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LDK.
+FOUNDATION_EXPORT double LDKVersionNumber;
+
+//! Project version string for LDK.
+FOUNDATION_EXPORT const unsigned char LDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDK/PublicHeader.h>
+
+

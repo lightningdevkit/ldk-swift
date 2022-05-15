3e21cfbd03afe7c03b0d046cfe04321793dfa90d
Jurvis Tan <hello@jurvis.co>
2022-05-15 07:36:56 -0700

    Add shell script to build frameworks

---
+++ xcode/LDK/build-frameworks.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -e # abort if any command fails
+
+BIN_OUTPUT_DIRECTORY="`pwd`/build"
+
+rm -rf $BIN_OUTPUT_DIRECTORY
+
+xcodebuild archive -scheme LDKFramework \
+  -destination "generic/platform=iOS Simulator" \
+  -archivePath ${BIN_OUTPUT_DIRECTORY}/LDKFramework-Sim \
+  SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild archive -scheme LDKFramework \
+  -destination "generic/platform=iOS" \
+  -archivePath ${BIN_OUTPUT_DIRECTORY}/LDKFramework-iOS \
+  SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild -create-xcframework \
+  -framework ${BIN_OUTPUT_DIRECTORY}/LDKFramework-iOS.xcarchive/Products/Library/Frameworks/LDKFramework.framework \
+  -framework ${BIN_OUTPUT_DIRECTORY}/LDKFramework-Sim.xcarchive/Products/Library/Frameworks/LDKFramework.framework \
+  -output ${BIN_OUTPUT_DIRECTORY}/LDKFramework.xcframework

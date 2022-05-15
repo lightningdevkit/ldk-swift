75f4a403621fd523622b85fc609300a4e95b62ac
Jurvis Tan <hello@jurvis.co>
2022-05-15 10:59:02 -0700

    Name build frameworks scripts

--- xcode/LDK/build-frameworks-ios.sh
+++ xcode/LDK/build-frameworks-ios.sh
@@ -5,10 +5,14 @@ BIN_OUTPUT_DIRECTORY="`pwd`/build"
 
 rm -rf $BIN_OUTPUT_DIRECTORY
 
+rm -f libldk.a
+
 xcodebuild archive -scheme LDKFramework \
   -destination "generic/platform=iOS Simulator" \
   -archivePath ${BIN_OUTPUT_DIRECTORY}/LDKFramework-Sim \
   SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+rm -f libldk.a
 
 xcodebuild archive -scheme LDKFramework \
   -destination "generic/platform=iOS" \

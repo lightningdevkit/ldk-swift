4e2db6d0f36098d392726f68be57780668d954ce
Arik Sosman <git@arik.io>
2022-06-24 17:27:31 -0700

    Fix Package.swift include behavior for Mac

---
+++ ci/LDKSwift/Libraries/arm64/README.md
@@ -0,0 +1,3 @@
+# Dynamic Compilation Instructions
+
+// todo
--- ci/LDKSwift/Package.swift
+++ ci/LDKSwift/Package.swift
@@ -6,80 +6,63 @@ import Foundation
 
 var cBindingsBase = "/Users/arik/Developer/ldk-c-bindings"
 if let bindingsBase = getenv("LDK_C_BINDINGS_BASE") {
-    cBindingsBase = String(utf8String: bindingsBase)!
+	cBindingsBase = String(utf8String: bindingsBase)!
 }
 
-var fileExtension = ".dylib"
-#if os(Linux)
-fileExtension = ".a"
-#endif
-
 var linkerSettings: [PackageDescription.LinkerSetting] = [
-    .linkedLibrary("\(cBindingsBase)/lightning-c-bindings/target/debug/libldk\(fileExtension)")
+	.linkedLibrary("\(cBindingsBase)/lightning-c-bindings/target/debug/libldk.a")
 ]
 
-#if os(Linux)
-    linkerSettings.append(.linkedLibrary(String(utf8String: getenv("LLVM_CLANG_ASAN_PATH")!)!))
+#if os(macOS)
+	linkerSettings = [
+		.unsafeFlags(["-L\(cBindingsBase)/lightning-c-bindings/target/debug"]),
+		.linkedLibrary("ldk")
+	]
+#elseif os(Linux)
+	linkerSettings.append(.linkedLibrary(String(utf8String: getenv("LLVM_CLANG_ASAN_PATH")!)!, .when(platforms: [.linux])))
 #endif
 
 let package = Package(
-        name: "LDKSwift",
-        products: [
-            .library(
-                    name: "LDKSwift",
-                    targets: ["LDKSwift"]),
-            // .library(name: "LDKSwift", type: .dynamic, targets: ["LDKSwift"])
-        ],
-        dependencies: [
-            .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"3.0.0")
-        ],
-        targets: [
-            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-            // Targets can depend on other targets in this package, and on products in packages this package depends on.
-            .target(name: "LDKHeaders"),
-            // .systemLibrary(name: "LDKHeaders"),
-            .target(
-                    name: "LDKSwift",
-                    dependencies: [
-                        "LDKHeaders"
-                    ],
-                    path: nil,
-                    exclude: [],
-                    sources: nil,
-                    // sources: [
-                    //     "ldk_net.h",
-                    //     "ldk_net.c",
-                    //     "ldk_rust_types.h",
-                    //     "ldk_ver.h",
-                    //     "lightning.h",
-                    //     "SanitySample.swift"
-                    // ],
-                    resources: nil,
-                    publicHeadersPath: "include",
-                    cSettings: nil,
-                    // cSettings: [
-                    //     .headerSearchPath("include"),
-                    // ],
-                    cxxSettings: nil,
-                    swiftSettings: nil,
-                    linkerSettings: linkerSettings
-            ),
-            .testTarget(
-                    name: "LDKSwiftTests",
-                    dependencies: [
-                        "LDKSwift",
-                        "LDKHeaders",
-                        .product(name: "Crypto", package: "swift-crypto")
-                    ],
-                    path: nil,
-                    exclude: [],
-                    // exclude: ["SampleTest.swift"],
-                    sources: nil,
-                    // sources: [
-                    //     "SampleTest.swift"
-                    // ],
-                    cSettings: nil, cxxSettings: nil, swiftSettings: nil,
-                    linkerSettings: nil)
+	name: "LDKSwift",
+	products: [
+		.library(
+			name: "LDKSwift",
+			targets: ["LDKSwift"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"3.0.0")
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(name: "LDKHeaders"),
+		.target(
+			name: "LDKSwift",
+			dependencies: [
+				"LDKHeaders"
+			],
+			path: nil,
+			exclude: [],
+			sources: nil,
+			resources: nil,
+			publicHeadersPath: "include",
+			cSettings: nil,
+			cxxSettings: nil,
+			swiftSettings: nil,
+			linkerSettings: linkerSettings
+		),
+		.testTarget(
+			name: "LDKSwiftTests",
+			dependencies: [
+				"LDKSwift",
+				"LDKHeaders",
+				.product(name: "Crypto", package: "swift-crypto")
+			],
+			path: nil,
+			exclude: [],
+			sources: nil,
+			cSettings: nil, cxxSettings: nil, swiftSettings: nil,
+			linkerSettings: nil)
 
-        ]
+	]
 )

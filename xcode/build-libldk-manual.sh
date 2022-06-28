10350b97ed2d8eb11d087ce513497dd9618a402b
Arik Sosman <git@arik.io>
2022-06-27 18:42:14 -0700

    fix absolute path detection, select xcode 13.2.1 for everything, and use local paths for lipo input

--- xcode/build-libldk-manual.sh
+++ xcode/build-libldk-manual.sh
@@ -25,25 +25,35 @@ set -e # stop execution upon the first error
 
 C_BINDINGS_SOURCE_DIRECTORY="$(cd ${LDK_DIRECTORY}; pwd)/lightning-c-bindings"
 
-BASEDIR=$(dirname "$0")
-BUILT_PRODUCTS_DIR="${BASEDIR}/binaries" # directory to copy the shared library and headers into
+# https://stackoverflow.com/a/4774063/299711
+BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+BUILT_PRODUCTS_DIR="${BASEDIR}/binaries" # directory to copy the shared library and headers into
+BUILD_LOG_PATH="${BASEDIR}/build_libldk.log"
 TARGET_NAME="libldk"
+
+# initialize the build log
+echo -n "" > $BUILD_LOG_PATH
 
 declare -a platforms=("iphoneos" "iphonesimulator" "macosx" "macosx")
 declare -a llvm_target_triple_suffixes=("" "-simulator" "" "-macabi")
-declare -a architectures=( "arm64" "arm64,x86_64" "arm64,x86_64" "arm64,x86_64" )
+declare -a architectures=( "arm64" "arm64 x86_64" "arm64 x86_64" "arm64 x86_64" )
 
 for (( i=0; i<4; i++ ));
 do
     PLATFORM_NAME=${platforms[$i]}
+    PLATFORM_HUMAN_NAME="${PLATFORM_NAME}"
     LLVM_TARGET_TRIPLE_SUFFIX=${llvm_target_triple_suffixes[$i]}
-	concatenated_architectures=${architectures[$i]}
-	IFS=',' read -r -a ARCHS <<< "${concatenated_architectures}"
+	ARCHS=${architectures[$i]}
+
+	# sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+	sudo xcode-select -s /Applications/Xcode\ 13.2.1.app/Contents/Developer/
 
 	if [[ $PLATFORM_NAME = "macosx" ]]; then
 		if [[ $LLVM_TARGET_TRIPLE_SUFFIX = "-macabi" ]]; then
 			RUST_TARGET_OS="ios-macabi"
+			PLATFORM_HUMAN_NAME="catalyst"
+			# sudo xcode-select --switch /Library/Developer/CommandLineTools
 		else
 			RUST_TARGET_OS="darwin"
 		fi
@@ -53,18 +63,6 @@ do
 
 	# Clean any pre-existing static libraries that doesn't align with what we're currently trying to build
 
-	INDIVIDUAL_ARCH_BINARY_DIR="${BUILT_PRODUCTS_DIR}/${PLATFORM_NAME}/raw"
-	LIPO_BINARY_DIR="${BUILT_PRODUCTS_DIR}/${PLATFORM_NAME}/lipo"
-
-	for ARCH in $ARCHS
-	do
-		CURRENT_ARCH_BINARY_DIR="${INDIVIDUAL_ARCH_BINARY_DIR}/${ARCH}"
-		echo "Current architecture directory: ${CURRENT_ARCH_BINARY_DIR}"
-		if [[ $(lipo -info "${CURRENT_ARCH_BINARY_DIR}/${TARGET_NAME}" 2>&1) != *"${ARCH}"* ]]; then
-			rm -f "${CURRENT_ARCH_BINARY_DIR}/${TARGET_NAME}"
-		fi
-	done
-
 	if [[ $CONFIGURATION = "Debug" ]]; then
 		RUST_CONFIGURATION="debug"
 		RUST_CONFIGURATION_FLAG=""
@@ -72,6 +70,27 @@ do
 		RUST_CONFIGURATION="release"
 		RUST_CONFIGURATION_FLAG="--release"
 	fi
+
+	echo "Platform name: ${PLATFORM_NAME}" >> $BUILD_LOG_PATH
+    echo "Configuration: ${RUST_CONFIGURATION}" >> $BUILD_LOG_PATH
+    echo "LLVM Target Triple Suffix: ${LLVM_TARGET_TRIPLE_SUFFIX}" >> $BUILD_LOG_PATH
+
+	INDIVIDUAL_ARCH_BINARY_DIR="${BUILT_PRODUCTS_DIR}/${RUST_CONFIGURATION}/${PLATFORM_HUMAN_NAME}/raw"
+	LIPO_BINARY_DIR="${BUILT_PRODUCTS_DIR}/${RUST_CONFIGURATION}/${PLATFORM_HUMAN_NAME}/lipo"
+
+	for ARCH in $ARCHS
+	do
+		CURRENT_ARCH_BINARY_DIR="${INDIVIDUAL_ARCH_BINARY_DIR}/${ARCH}"
+		echo "Current architecture: ${ARCH}" >> $BUILD_LOG_PATH
+		echo "Current architecture lipo input directory: ${CURRENT_ARCH_BINARY_DIR}" >> $BUILD_LOG_PATH
+
+		# if whatever this weird lipo check is true
+		if [[ $(lipo -info "${CURRENT_ARCH_BINARY_DIR}/${TARGET_NAME}" 2>&1) != *"${ARCH}"* ]]; then
+			# remove the pre-lipo/raw/lipo-input binary
+			rm -f "${CURRENT_ARCH_BINARY_DIR}/${TARGET_NAME}"
+			# why do we need to remove it? Who knows
+		fi
+	done
 
 	pushd $C_BINDINGS_SOURCE_DIRECTORY
 	export RUSTFLAGS='--cfg=c_bindings'
@@ -96,18 +115,24 @@ do
 			fi
 		fi
 
-		echo "BUILDING ${RUST_ARCH}-apple-${RUST_TARGET_OS}"
-
+		echo "BUILDING ${RUST_ARCH}-apple-${RUST_TARGET_OS}" >> $BUILD_LOG_PATH
 		cargo build -Z build-std=panic_abort,std --features "std" --target "${RUST_ARCH}-apple-${RUST_TARGET_OS}" $RUST_CONFIGURATION_FLAG
-		EXECUTABLES+=("$C_BINDINGS_SOURCE_DIRECTORY/target/${RUST_ARCH}-apple-${RUST_TARGET_OS}/${RUST_CONFIGURATION}/${TARGET_NAME}.a")
+		echo "Finished building ${RUST_ARCH}-apple-${RUST_TARGET_OS}" >> $BUILD_LOG_PATH
 
 		mkdir -p "${CURRENT_ARCH_BINARY_DIR}"
 		cp "$C_BINDINGS_SOURCE_DIRECTORY/target/${RUST_ARCH}-apple-${RUST_TARGET_OS}/${RUST_CONFIGURATION}/${TARGET_NAME}.a" "${CURRENT_ARCH_BINARY_DIR}"
+		EXECUTABLES+=("${CURRENT_ARCH_BINARY_DIR}/${TARGET_NAME}.a")
 	done
 
 	rustup override unset
 
 	mkdir -p "${LIPO_BINARY_DIR}"
+
+	echo "Current platform lipo output directory: ${LIPO_BINARY_DIR}" >> $BUILD_LOG_PATH
+	echo "LIPOING ${PLATFORM_NAME}" >> $BUILD_LOG_PATH
 	xcrun --sdk $PLATFORM_NAME lipo -create "${EXECUTABLES[@]}" -output "${LIPO_BINARY_DIR}/${TARGET_NAME}.a"
+	echo "Finished lipoing ${PLATFORM_NAME}" >> $BUILD_LOG_PATH
+
+	echo "" >> $BUILD_LOG_PATH
 
 done

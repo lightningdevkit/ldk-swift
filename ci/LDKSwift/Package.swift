8b951652c24ce70f1389ecb845d1d43d6cf9f59c
Arik Sosman <git@arik.io>
2022-06-28 11:27:03 -0700

    make LDKSwift package dynamic, and always use local derived data paths to avoid cache conflicts

--- ci/LDKSwift/Package.swift
+++ ci/LDKSwift/Package.swift
@@ -39,7 +39,7 @@ let package = Package(
 	products: [
 		.library(
 			name: "LDKSwift",
-			// type: .dynamic,
+			type: .dynamic,
 			targets: ["LDKSwift"]),
 	],
 	dependencies: [
--- ci/build_frameworks.sh
+++ ci/build_frameworks.sh
@@ -23,8 +23,8 @@ fi
 
 XCFRAMEWORK_INPUT_FLAGS=""
 
-declare -a destinationNames=( "iOS Simulator" "iOS" )
-declare -a lipoDirectoryNames=( "iphonesimulator" "iphoneos" )
+declare -a destinationNames=( "iOS Simulator" "iOS" "OS X" "macOS,variant=Mac Catalyst" )
+declare -a lipoDirectoryNames=( "iphonesimulator" "iphoneos" "macosx" "catalyst" )
 
 declare archiveCount=${#lipoDirectoryNames[@]}
 for (( i=0; i<$archiveCount; i++ ));
@@ -32,6 +32,7 @@ do
 	CURRENT_DESTINATION_NAME=${destinationNames[$i]}
 	CURRENT_LIPO_DIRECTORY_NAME_INFIX=${lipoDirectoryNames[$i]}
 	CURRENT_ARCHIVE_DIRECTORY="${BASEDIR}/../bindings/bin/${RUST_CONFIGURATION}/${CURRENT_LIPO_DIRECTORY_NAME_INFIX}/xcarchive"
+	CURRENT_DERIVED_DATA_DIRECTORY="${BASEDIR}/../bindings/bin/${RUST_CONFIGURATION}/${CURRENT_LIPO_DIRECTORY_NAME_INFIX}/DerivedData"
 	CURRENT_ARCHIVE_PATH="${CURRENT_ARCHIVE_DIRECTORY}/${CURRENT_LIPO_DIRECTORY_NAME_INFIX}"
 
 	CURRENT_LIPO_DIRECTORY_PATH="${BUILD_PRODUCTS_DIR}/${RUST_CONFIGURATION}/${CURRENT_LIPO_DIRECTORY_NAME_INFIX}/lipo"
@@ -39,14 +40,21 @@ do
 
 	echo "Building xcarchive for ${CURRENT_DESTINATION_NAME}" >> $BUILD_LOG_PATH
 	echo "Current lipo input directory: ${CURRENT_LIPO_DIRECTORY_PATH}" >> $BUILD_LOG_PATH
-	echo "Current xcarchive archive directory: ${CURRENT_ARCHIVE_PATH}" >> $BUILD_LOG_PATH
+	echo "Current derived data directory: ${CURRENT_DERIVED_DATA_DIRECTORY}" >> $BUILD_LOG_PATH
+	echo "Current xcarchive output directory: ${CURRENT_ARCHIVE_PATH}" >> $BUILD_LOG_PATH
 
 	mkdir -p "${CURRENT_ARCHIVE_DIRECTORY}"
+	mkdir -p "${CURRENT_DERIVED_DATA_DIRECTORY}"
+	find "${CURRENT_ARCHIVE_DIRECTORY}" -mindepth 1 -delete
+	find "${CURRENT_DERIVED_DATA_DIRECTORY}" -mindepth 1 -delete
 
 	pushd "${BASEDIR}/LDKSwift"
-	xcodebuild archive -scheme LDKSwift -destination "generic/platform=${CURRENT_DESTINATION_NAME}" -archivePath "${CURRENT_ARCHIVE_PATH}" ENABLE_BITCODE=NO SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+	xcodebuild archive -scheme LDKSwift -destination "generic/platform=${CURRENT_DESTINATION_NAME}" -derivedDataPath "${CURRENT_DERIVED_DATA_DIRECTORY}" -archivePath "${CURRENT_ARCHIVE_PATH}" ENABLE_BITCODE=NO EXCLUDED_ARCHS="i386 armv7" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+	# xcodebuild -scheme LDKSwift -destination "generic/platform=${CURRENT_DESTINATION_NAME}" ENABLE_BITCODE=NO SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 	popd
 
-	XCFRAMEWORK_INPUT_FLAGS="${XCFRAMEWORK_INPUT_FLAGS}-framework ${CURRENT_ARCHIVE_PATH}.xcarchive/Products/Library/Frameworks/LDKSwift.framework "
+	# XCFRAMEWORK_INPUT_FLAGS="${XCFRAMEWORK_INPUT_FLAGS}-framework ${CURRENT_ARCHIVE_PATH}.xcarchive/Products/Library/Frameworks/LDKSwift.framework "
+	XCFRAMEWORK_INPUT_FLAGS="${XCFRAMEWORK_INPUT_FLAGS}-framework ${CURRENT_ARCHIVE_PATH}.xcarchive/Products/usr/local/lib/LDKSwift.framework "
 	echo "Current xcframework flags: ${XCFRAMEWORK_INPUT_FLAGS}" >> $BUILD_LOG_PATH
+	echo "" >> $BUILD_LOG_PATH
 done

9638d0395d3606a39423113faf03a3cba1d50d58
Arik Sosman <git@arik.io>
2022-07-07 10:43:24 -0700

    Add header copying script.

---
+++ src/scripts/prepare_headers.py
@@ -0,0 +1,32 @@
+import os.path
+import subprocess
+
+from script_config import BuildConfig
+
+
+def run(config: BuildConfig):
+	header_directories = [
+		os.path.join(config.LDK_C_BINDINGS_DIRECTORY, 'include'),
+		os.path.join(config.LDK_C_BINDINGS_BASE, 'ldk-net'),
+	]
+
+	header_destination_directory = os.path.realpath(os.path.join(os.path.dirname(__file__), '../../xcode/LDKFramework/headers'))
+	os.makedirs(header_destination_directory, exist_ok=True)
+
+	for current_directory in header_directories:
+		for current_file in os.listdir(current_directory):
+
+			is_relevant_file = current_file.endswith('.h') or current_file.endswith('.c')
+			if not is_relevant_file:
+				continue
+
+			current_path = os.path.join(current_directory, current_file)
+			if not os.path.isfile(current_path):
+				continue
+
+			subprocess.check_call(['cp', current_path, header_destination_directory])
+
+
+if __name__ == '__main__':
+	config = BuildConfig.parse(allow_ldk_argument=True)
+	run(config)

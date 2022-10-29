d1fce6972c24babddf8773bdb2954925968cd71a
runner <runner@Mac-1667074697938.local>
2022-10-29 21:29:21 +0000

    Update Package.swift for 0.0.112 release.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let tag = "0.0.111"
-let checksum = "6eb059e8785832b23d3ad6a374d0ce107795372771a70e006b516ebf5f80ac1a"
+let tag = "0.0.112"
+let checksum = "b7a005bacdba5bfcd119004989e9c5ce25894d83c054b0f62d3e136c76186018"
 let url = "https://github.com/lightningdevkit/ldk-swift/releases/download/\(tag)/LightningDevKit.xcframework.zip"
 
 let package = Package(
